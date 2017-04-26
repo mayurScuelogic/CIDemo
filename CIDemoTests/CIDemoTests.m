d9e3f7ddd106cca1b2866ea79f744a9ad8718353
Sagar Kudale <sagar.kudale@cuelogic.co.in>
2017-04-26 12:13:36 +0530

    Sachin Demo

--- CIDemoTests/CIDemoTests.m
+++ CIDemoTests/CIDemoTests.m
@@ -27,7 +27,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssertTrue(false , @"Mayur");
+    XCTAssertTrue(false , @"Sachin");
 }
 
 - (void)testPerformanceExample {
